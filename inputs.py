15c36ff942dc957138a712d1e162251ad4ff8c6c
Oranfreezah <80894282+Oranfreezah@users.noreply.github.com>
2021-04-14 16:05:55 +0200

    Updated ADD function
    Switched to CSV for better file manipulation, completed adding to file

--- inputs.py
+++ inputs.py
@@ -5,10 +5,10 @@ Created on Mon Mar 22 10:52:39 2021
 @author: Oranf_fjox16m
 """
 from colorama import Fore, Style
-import json
+import csv
 
 def menu():
-    
+    filecheck()
     comm_list = ['search', 'add', 'mod', 'quit']
     
     print('-----------------------')
@@ -56,8 +56,8 @@ def menu():
         
 def check_existence(pass_library):
     
-    with open('F:/Archivio/Python/prova.json', 'r') as archive:
-            file_check = json.load(archive)
+    with open('F:/Archivio/Python/prova.csv', 'r') as archive:
+            file_check = csv.reader(archive)
             for x in file_check:
                 if pass_library in file_check:
                     return True
@@ -65,26 +65,59 @@ def check_existence(pass_library):
                     return False
     
 
+def command_list():
+    comm_dict = {}
+    while True:
+        comm = input('New command. Input q to stop: ')
+        if comm == 'q':
+            break
+        else:
+            comm_fun = input('Insert command description: ')
+            comm_dict.update({comm:comm_fun})
+            print(comm_dict)
+    
+    return comm_dict
+            
+            
+def filecheck():
+    try:
+        f = open('F:/Archivio/Python/prova.csv')   
+    except IOError:
+        print('File not found. Creating new file')
+        open('F:/Archivio/Python/prova.csv', 'w')
+                 
+            
+        
+
 def add():
     
     lib = input('New library: ')
     check = check_existence(lib)
     
+    
     if check == True:
         print('True')
     else:
-        print(f'Adding {lib} as new library.')
-        print()
         desc = input('insert description: ')
-        comm = input('insert commands: ')
-        #qui ci sarà il dizionario da salvare sul file
+        comm_list = command_list()
         
         
-            
+    new_library = {"Name": lib,
+                   "Use": desc,
+                   "Commands": comm_list}
+    with open('F:/Archivio/Python/prova.csv', 'w', newline="") as archive:
+        fields = ["Name", "Use", "Commands"]
+        writer = csv.DictWriter(archive, fieldnames=fields)
+        writer.writeheader()
+        writer.writerow(new_library)
+    
+    with open('F:/Archivio/Python/prova.csv', 'r') as archive:
+        reader = csv.reader(archive)
+        line_count = 0
+        for row in reader:
+            print(row)
         
-            
-   
-        
+
 add()  
 
     
